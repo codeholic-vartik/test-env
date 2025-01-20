89128ca264b6829a59f88f5c62af82fe58d0decc
Vartik Anand <codeholic.vartik@gmail.com>
2025-01-20 13:12:57 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
   // Render the page content once client-side code is ready
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Test
+      Test CD/CI
       <div>
         <h1>Environment: {process.env.NEXT_PUBLIC_ENVIRONMENT}</h1>
         <p>API URL: {process.env.NEXT_PUBLIC_API_URL}</p>
